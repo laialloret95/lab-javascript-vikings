a50144b0d56483b994e4bfb9412812a87d0a8c01
Laia Lloret <laialloret95@gmail.com>
2021-03-01 09:06:27 +0100

    add class War

--- src/viking.js
+++ src/viking.js
@@ -15,8 +15,8 @@ class Soldier {
 // Viking
 class Viking extends Soldier {
     constructor(name,health,strength) {
-        super(health,strength),
-        this.name = name,
+        super(health,strength);
+        this.name = name;
     }
     receiveDamage(damage) {
         this.health -= damage;
@@ -38,4 +38,30 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+
+    addViking(Viking) {
+        this.vikingArmy.push(Viking);
+    }
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon);
+    }
+    vikingAttack() {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * saxonArmy.length)];
+        let randomViking = this.saxonArmy[Math.floor(Math.random() * saxonArmy.length)];
+        
+        let result = randomSaxon.receiveDamage(randomViking.strength);
+        if (randomSaxon.health <= 0) { this.saxonArmy.splice(randomSaxon,1) }
+        return result;
+    }
+    saxonAttack() {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * saxonArmy.length)];
+        let randomViking = this.saxonArmy[Math.floor(Math.random() * saxonArmy.length)];
+        
+        let result = randomViking.receiveDamage(randomSaxon.strength);
+        if (randomViking.health <= 0) { this.randomViking.splice(randomViking,1) }
+        return result;
+    }
+}
